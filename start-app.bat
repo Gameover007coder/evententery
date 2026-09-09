ee6953f1464edbf4691908400f5662563ee09bd9
UJJAWAL SINGH <182491778+Gameover007coder@users.noreply.github.com>
2026-09-09 09:39:01 +0530

    Delete start-app.bat

--- start-app.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Event QR App - Admin Scanner
-echo ========================================================
-echo         STARTING EVENT QR PASS & ADMIN SCANNER
-echo ========================================================
-cd /d "%~dp0"
-node server.js
-pause
